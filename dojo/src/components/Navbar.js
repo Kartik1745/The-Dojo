749d12ecb9a3269a6b320f1af32fb93651cd16f3
Kartik1745 <kartikpatil1997@gmail.com>
2023-02-07 20:40:48 -0500

    Creating navbar component

---
+++ dojo/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Era Echoes</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/">Blogs</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
