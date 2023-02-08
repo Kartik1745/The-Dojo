a3b4570d53210686592457b9250f9bc40fd67c2e
Kartik1745 <kartikpatil1997@gmail.com>
2023-02-07 20:41:22 -0500

    Creating About

---
+++ dojo/src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div className="about">
+        <h1>Exploring the story of humanity, one event at a time.</h1>
+        </div>
+    );
+}
+
+export default About;
