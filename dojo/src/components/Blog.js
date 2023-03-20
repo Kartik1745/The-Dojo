1556b5000aae5978d7b8a65356dc798552d341f2
Kartik1745 <kartikpatil1997@gmail.com>
2023-03-20 18:56:55 -0400

    Adding Delete button

--- dojo/src/components/Blog.js
+++ dojo/src/components/Blog.js
@@ -1,22 +1,22 @@
 import { useState } from "react";
 import './Blog.css';
+import BlogList from "./BlogList";
 
-const Home = () => {
-    const [blogs, setBlog] = useState([
+const Blog = () => {
+    const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id!==id);
+        setBlogs(newBlogs);
+    }
     return (
-        <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-                ))}
+        <div className="blog">
+            <BlogList blogs={blogs} handleDelete={handleDelete} />
         </div>    
     );
 }
 
-export default Home;
+export default Blog;
