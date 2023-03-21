19b067844621382acc1635f28aec9c256595371b
Kartik1745 <kartikpatil1997@gmail.com>
2023-03-21 15:24:29 -0400

    Creating DB

--- dojo/src/components/Blog.js
+++ dojo/src/components/Blog.js
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './Blog.css';
 import BlogList from "./BlogList";
 
 const Blog = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
     const handleDelete = (id) =>{
         const newBlogs = blogs.filter(blog => blog.id!==id);
         setBlogs(newBlogs);
     }
+    
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs').then(
+            res => {
+               return res.json()
+            }
+        ).then(
+            data =>{
+                console.log(data);
+                setBlogs(data);
+            }
+        )
+    },[])
+
     return (
         <div className="blog">
-            <BlogList blogs={blogs} handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} />}
         </div>    
     );
 }
